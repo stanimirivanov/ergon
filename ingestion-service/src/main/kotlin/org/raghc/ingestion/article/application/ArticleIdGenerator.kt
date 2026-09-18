ac93a750eff34c00e1fbf2f982b8d03a5fa5a773
Stanimir Ivanov <stanimr@protonmail.com>
2026-09-18 16:09:02 +0200

    [#108] Retire predecessor ingestion service

--- ingestion-service/src/main/kotlin/org/raghc/ingestion/article/application/ArticleIdGenerator.kt
+++
@@ -1,7 +0,0 @@
-package org.raghc.ingestion.article.application
-
-import org.raghc.ingestion.article.domain.ArticleId
-
-fun interface ArticleIdGenerator {
-    fun next(): ArticleId
-}
--- ingestion-service/src/main/resources/db/migration/V4__add_article_collection_membership.sql
+++
@@ -1,4 +0,0 @@
-alter table article_projection add column collection_id uuid;
-
-create index article_projection_collection_idx
-    on article_projection (tenant_id, collection_id, article_id);
